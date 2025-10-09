97b164bf2bdd308b8cb45f87b77df5c360fa510f
Michael Baselios <126206406+michaelbasileios@users.noreply.github.com>
2025-10-09 15:00:08 -0500

    Reformat HTML to use explicit labels

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "./styles.css";
 
 const form = document.querySelector("form");
+const formInputs = document.querySelectorAll(".form-input");
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email + span.error");
+
+formInputs.forEach((inputElement) => {
+  inputElement.addEventListener("input", (e) => {});
+});
+// email.addEventListener("input", (e) => {
+//   if (email.validity.valid) {
+//     emailError.textContent = ""
+//   }
+// });
